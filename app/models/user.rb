8e28d83c7f4860478cb14e373d46c32720dc3c81
vlezya <vlezyashop@gmail.com>
2021-11-19 01:11:59 +0200

    added validation for username

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,28 +4,25 @@ class User < ApplicationRecord
   # Constants
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
-  REGEX_USERNAME = /\A[a-zA-Z0–9 _]+\z/
-  REGEX_EMAIL = /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/
+  VALID_USERNAME_REGEX = /\A[a-zA-Z0–9_]+\z/i
+  VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   
   # Associations
   has_many :questions, dependent: :destroy
   
   # Callbacks
   before_save :encrypt_password
+  before_save :generate_lowercase_username
   
   # Validations
-  validates :username, presence: true, length: { maximum: 40 }, format: { with: REGEX_USERNAME }
-  validates :email, presence: true, uniqueness: true, format: { with: REGEX_EMAIL }
-  validates :password, presence: true, on: :create, confirmation: { case_sensitive: true }
+  validates :username, presence: true, length: { maximum: 40 }, format: { with: VALID_USERNAME_REGEX }
+  validates :email, presence: true, uniqueness: true, format: { with: VALID_EMAIL_REGEX }
+  validates :password,  presence: true, on: :create, confirmation: { case_sensitive: true }
   
-  # Setter
+  # Getters and Setters
   attr_accessor :password
-    
-  def hash_to_string(password_hash)
-    password_hash.unpack('H*')[0]
-  end
-  
-  def authenticate(email, password)
+
+  def self.authenticate(email, password)
     user = find_by(email: email)
     return unless user.present?
     hashed_password = User.hash_to_string(
@@ -41,9 +38,19 @@ class User < ApplicationRecord
     def encrypt_password
       if password.present?
         password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-        password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
-          password, password_salt, ITERATIONS, DIGEST.length, DIGEST)
+        password_hash = User.hash_to_string(
+          OpenSSL::PKCS5.pbkdf2_hmac(
+            password, password_salt, ITERATIONS, DIGEST.length, DIGEST
+          )
         )
       end
+    end
+
+    def self.hash_to_string(password_hash)
+      password_hash.unpack('H*')[0]
+    end
+      
+    def generate_lowercase_username
+      self.username.downcase!
     end
 end

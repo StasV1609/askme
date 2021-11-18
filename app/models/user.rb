c2bd04d13d3aa6f35218f1c4c081c7872474fa6c
vlezya <vlezyashop@gmail.com>
2021-11-18 22:19:30 +0200

    Edited User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,8 @@ class User < ApplicationRecord
   # Constants
   ITERATIONS = 20_000
   DIGEST = OpenSSL::Digest::SHA256.new
+  REGEX_USERNAME = /\A[a-zA-Z0–9 _]+\z/
+  REGEX_EMAIL = /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/
   
   # Associations
   has_many :questions, dependent: :destroy
@@ -12,37 +14,36 @@ class User < ApplicationRecord
   before_save :encrypt_password
   
   # Validations
-  validates :username, presence: true, length: { maximum: 40 },
-                    format: { with: /\A[a-zA-Z0-9](\w|\.)*[a-zA-Z0-9]+\z/ }
-  validates :email, presence: true, uniqueness: true,
-                    format: { with: /\A[^@\s]+@([^@.\s]+\.)*[^@.\s]+\z/ }
-  validates_presence_of :password, on: :create
-  validates_confirmation_of :password
+  validates :username, presence: true, length: { maximum: 40 }, format: { with: REGEX_USERNAME }
+  validates :email, presence: true, uniqueness: true, format: { with: REGEX_EMAIL }
+  validates :password, presence: true, on: :create, confirmation: { case_sensitive: true }
   
+  # Setter
   attr_accessor :password
-  
-  def encrypt_password
-    if self.password.present?
-      self.password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
-      self.password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
-        self.password, self.password_salt, ITERATIONS, DIGEST.length, DIGEST)
-      )
-    end
-  end
-  
-  def self.hash_to_string(password_hash)
+    
+  def hash_to_string(password_hash)
     password_hash.unpack('H*')[0]
   end
   
-  def self.authenticate(email, password)
+  def authenticate(email, password)
     user = find_by(email: email)
-    return nil unless user.present?
+    return unless user.present?
     hashed_password = User.hash_to_string(
       OpenSSL::PKCS5.pbkdf2_hmac(
         password, user.password_salt, ITERATIONS, DIGEST.length, DIGEST
       )
     )
     return user if user.password_hash == hashed_password
-    nil
   end
+  
+  private
+  
+    def encrypt_password
+      if password.present?
+        password_salt = User.hash_to_string(OpenSSL::Random.random_bytes(16))
+        password_hash = User.hash_to_string(OpenSSL::PKCS5.pbkdf2_hmac(
+          password, password_salt, ITERATIONS, DIGEST.length, DIGEST)
+        )
+      end
+    end
 end

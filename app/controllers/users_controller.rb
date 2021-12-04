b25d2d0f56b6e8eeac8bf5b05ece1c7d582ac994
vlezya <vlezyashop@gmail.com>
2021-12-04 15:56:42 +0200

    Added answers to questions

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,6 +12,11 @@ class UsersController < ApplicationController
 
   def create
     @user = User.new(user_params)
+    if @user.save
+      redirect_to root_url, notice: 'Пользователь успешно зарегестрирован'
+    else
+      render :new
+    end
   end
 
   def show
@@ -27,6 +32,10 @@ class UsersController < ApplicationController
     ]
 
     @questions_amount = @questions.count
+    @questions_amount = @questions.count
+    @answers_amount = @questions.count(&:answer)
+    @unanswered_amount = @questions_amount - @answers_amount
+
     @new_question = Question.new
   end
 

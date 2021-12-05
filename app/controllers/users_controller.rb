47750bcf73099fb8e362c2d7f26cafa4b585bd6a
vlezya <vlezyashop@gmail.com>
2021-12-05 19:33:17 +0200

    added authenicate & authorization

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,14 @@
 class UsersController < ApplicationController
+
+  before_action :set_user, only: %i[edit update show]
+  before_action :authorize_user, except: %i[create new show index]
+
   def index
     @users = User.new(id: 1, name: 'Stas', username: 'Stas1609'), User.new(id: 2, name: 'Artem', username: 'Art12')
   end
 
   def new
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
     @user = User.new
   end
 
@@ -11,6 +16,8 @@ class UsersController < ApplicationController
   end
 
   def create
+    redirect_to root_url, alert: 'Вы уже залогинены' if current_user.present?
+
     @user = User.new(user_params)
     if @user.save
       redirect_to root_url, notice: 'Пользователь успешно зарегестрирован'
@@ -19,11 +26,18 @@ class UsersController < ApplicationController
     end
   end
 
+  def update
+    if @user.update(user_params)
+      redirect_to user_path(@user), notice: 'Данные обновлены'
+    else
+      render :edit
+    end
+  end
+
   def show
-    @user = User.new(
-      name: 'Stas',
-      username: 'Stas1609',
-    )
+    @uestions = @user.questions.order(created_at: :desc)
+
+    @new_question = @user.questions.build
 
     @questions = [
       Question.new(text: 'Как дела?', created_at: Date.parse('27.03.2016')),
@@ -32,7 +46,6 @@ class UsersController < ApplicationController
     ]
 
     @questions_amount = @questions.count
-    @questions_amount = @questions.count
     @answers_amount = @questions.count(&:answer)
     @unanswered_amount = @questions_amount - @answers_amount
 
@@ -40,6 +53,14 @@ class UsersController < ApplicationController
   end
 
   private
+    def authorize_user
+      reject_user unless user == current_user  
+    end
+
+    def set_user
+      @user ||= User.find(params[:id])
+    end
+
     def user_params
       params.require(:user).permit(:name, :username, :email, :password,
                                    :password_confirmation, :avatar_url)
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,10 @@
 Rails.application.routes.draw do
   root 'users#index'
   resources :users
+  resources :sessions, only: %i[new create destroy]
   resources :questions
+
+  get 'sign_up', to: 'users#new'
+  get 'log_out', to: 'sessions#destroy'
+  get 'log_in', to: 'sessions#new'
 end
